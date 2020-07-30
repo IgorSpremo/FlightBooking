be5e69fa9490e254d558c448f9ac468e6c056b54
Igor Spremo <igor.spremo@gmail.com>
2020-07-30 11:11:37 +0200

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import {AuthModule} from './auth/auth.module';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+@NgModule({
+  declarations: [LoginComponent, ResetPasswordComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ],
+  exports: [
+    LoginComponent, 
+    ResetPasswordComponent, 
+    RegisterComponent
+  ]
+})
+export class AuthModule { }
