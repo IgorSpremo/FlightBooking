8340f5ad664c01d83194742e316abe53712282cf
Igor Spremo <igor.spremo@gmail.com>
2020-08-03 16:15:02 +0200

    Added angular material navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import {AuthModule} from './auth/auth.module';
 import { from } from 'rxjs';
 import { ProfileComponent } from './profile/profile.component';
 import { DestinationComponent } from './destination/destination.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -21,11 +29,19 @@ import { DestinationComponent } from './destination/destination.component';
     GalleryComponent,
     ProfileComponent,
     DestinationComponent,
+    MainNavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
